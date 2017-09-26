436816430126e57581c187a58f1aaa7d157904c0
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-26 18:35:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  array.each_with_index do |element, index|
+    puts "#{index+1}. #{element}"
 end
-
+end 
 def summon_captain_planet# code an argument here
   # Your code here
 end
